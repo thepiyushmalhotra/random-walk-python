55af330b10ef46dde5e18f5a43124e29bdcabf14
Piyush Malhotra <46517096+thepiyushmalhotra@users.noreply.github.com>
2021-12-26 01:36:38 -0500

    replace one color

--- randomWalk.py
+++ randomWalk.py
@@ -4,7 +4,7 @@ import random
 yertle = Turtle()
 yertle.shape("circle")
 
-colours = ["#69FFF1","#2A324B","#EE6352","#F03A47","#E4FF1A", "#BA3B46", "#ED9B40", "#363946", "#139A43", "#FFE2D1"]
+colours = ["#69FFF1","#2A324B","#EE6352","#F03A47","#E4FF1A", "#BA3B46", "#ED9B40", "#363946", "#139A43", "#0FA3B1"]
 directions = [0, 90, 180, 270]
 
 for _ in range(500):
